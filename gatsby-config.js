c8dda436b279ec38388cbd8115315441d745345b
Shawn Lauzon <shawn.lauzon@gmail.com>
2020-06-07 12:26:03 +0700

    Set cookie domain

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,7 +65,7 @@ module.exports = {
         // Any additional optional fields
         sampleRate: 5,
         siteSpeedSampleRate: 10,
-        cookieDomain: "example.com",
+        cookieDomain: "evastrology.com",
       },
     },    
     // this (optional) plugin enables Progressive Web App + Offline functionality
